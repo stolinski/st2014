fe12f0e932b4f80229d6a58c811229e24d6a8d99
stolinski <scotttolinski@gmail.com>
2014-08-13 22:11:48 -0400

    adds new projects

--- public/js/collections/projects.js
+++ public/js/collections/projects.js
@@ -5,10 +5,13 @@ var ProjectsList = Backbone.Collection.extend({
 
 });
 
-var sig = new app.Project({ title: 'ACM SIGGRAPH', url: 'http://www.siggraph.org/', meta: '<p>Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built With: Drupal 7, Omega 3, Sass/Compass</p>'});
+var 
+	mat = new app.Project({ title: 'Material Colors', url: 'http://materialcolors.com/', meta: '<p>Built for: Fun<br />Built With: Angular.js, Polymer, Sass</p>'});
+	con = new app.Project({ title: 'Concrete.org', url: 'http://www.concrete.org/', meta: '<p>Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built Templates With: HTML5, CSS3, JavaScript</p>'});
+	sig = new app.Project({ title: 'ACM SIGGRAPH', url: 'http://www.siggraph.org/', meta: '<p>Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built With: Drupal 7, Omega 3, Sass/Compass</p>'});
     q = new app.Project({ title: 'Q LTD', url: 'http://qltd.com/', meta: '<p>Built for: <a target="_blank" href="http://qltd.com">Q LTD</a><br />Built With: Node.js, Express, MongoDB, Sass/Compass/Susy</p>' });
     check = new app.Project({ title: 'Dev Checklist', url: 'http://checklist.umichcreative.org/', meta: '<p>Built for: <a target="_blank" href="http://creative.umich.edu/">Michigan Creative</a><br />Built With: Angular.js, Sass/Compass</p>' });
     fac = new app.Project({ title: 'Operations Leadership Factory', url: 'http://factory.umichcreative.org/', meta: '<p>Built for: <a target="_blank" href="http://creative.umich.edu/">Michigan Creative</a><br />Built With: Drupal 7, Omega 4, Sass/Compass/SingularityGS</p>'  });
     lut = new app.Project({ title: 'Level Up Tuts', url: 'http://leveluptuts.com/', meta: '<p>450+ Web Dev Tutorials on: <a target="_blank" href="https://www.youtube.com/channel/UCyU5wkjgQYGRB0hIHMwm2Sg">Youtube</a><br />Built With: Drupal 7, Omega 4, Sass/Compass/SingularityGS</p>' });
 
-app.Projects = new ProjectsList([lut,q,sig,fac,check]);
+app.Projects = new ProjectsList([lut,mat,q,sig,fac,check,con]);
